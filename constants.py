0964a5a758caf78daf1ac7dc8579de1294603fa9
Jake Ross <jirhiker@gmail.com>
2022-12-21 14:26:50 -0700

    added downloads

--- constants.py
+++ constants.py
@@ -23,4 +23,36 @@ DEBUG_OBS = True
 DEBUG_OBS = False
 DEBUG_LIMIT_OBS = 0
 DEBUG_N_WELLS = 0
+
+MACROSTRAT_BM = {
+    "below": "traces",
+    "sourcetype": "raster",
+    "sourceattribution": '&copy; <a href="https://macrostrat.org">MacroStrat</a> contributors',
+    "source": ['http://tiles.macrostrat.org/carto/{z}/{x}/{y}.png']}
+
+USGS_BM = {
+    "below": "traces",
+    "sourcetype": "raster",
+    "sourceattribution": 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>',
+    "source": [
+        "https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}",
+    ],
+}
+
+ESRI_BM = {"below": "traces",
+           "sourcetype": "raster",
+           "sourceattribution": 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, '
+                                'GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+           'source': ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}']}
+OPENTOPO_BM = {"below": "traces",
+           "sourcetype": "raster",
+           "sourceattribution": 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> '
+                                '(<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+           'source': ['https://a.tile.opentopomap.org/{z}/{x}/{y}.png']}
+
+OSM_BM = {"below": "traces",
+           "sourcetype": "raster",
+           "sourceattribution": '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+           'source': ['https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png']}
+
 # ============= EOF =============================================
